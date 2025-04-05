c3fc6dc1bd74ed1fc524d3116413d958ccd8f603
Laiyuhung <laiyuhung921118@gmail.com>
2025-04-05 20:32:08 +0800

    更新

--- src/app/api/username/route.js
+++ src/app/api/username/route.js
@@ -1,14 +1,15 @@
+import supabase from '@/lib/supabase'
+
 export async function POST(req) {
-    const { user_id } = await req.json()
-  
-    const { data, error } = await supabase
-      .from('managers')
-      .select('name')
-      .eq('id', user_id)
-      .single()
-  
-    if (error) return Response.json({ error: error.message }, { status: 500 })
-  
-    return Response.json({ name: data.name })
-  }
-  
+  const { user_id } = await req.json()
+
+  const { data, error } = await supabase
+    .from('managers')
+    .select('name')
+    .eq('id', user_id)
+    .single()
+
+  if (error) return Response.json({ error: error.message }, { status: 500 })
+
+  return Response.json({ name: data.name })
+}

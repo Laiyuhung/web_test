00e890722955c169e54a87b4e96ce800df80e44b
Laiyuhung <laiyuhung921118@gmail.com>
2025-04-06 22:48:09 +0800

    更新

--- src/lib/supabaseServer.js
+++ src/lib/supabaseServer.js
@@ -1,12 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY
-
-if (!supabaseUrl || !supabaseServiceRoleKey) {
-  throw new Error('supabaseUrl and serviceRoleKey are required.')
-}
-
-const supabase = createClient(supabaseUrl, supabaseServiceRoleKey)
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+)
 
 export default supabase

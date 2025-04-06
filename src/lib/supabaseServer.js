ab392cf4b029a32369cdbc97884e0211a77518c4
Laiyuhung <laiyuhung921118@gmail.com>
2025-04-06 22:54:38 +0800

    更新

--- src/lib/supabaseServer.js
+++ src/lib/supabaseServer.js
@@ -4,5 +4,5 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.SUPABASE_SERVICE_ROLE_KEY
 )
-
+console.log('ROLE KEY:', process.env.SUPABASE_SERVICE_ROLE_KEY)
 export default supabase

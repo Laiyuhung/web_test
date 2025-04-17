e90b1695e6856b8fc145e5de4109d2d4d8108e47
Laiyuhung <laiyuhung921118@gmail.com>
2025-04-17 16:31:06 +0800

    更新0416

--- src/app/api/weekly_stats_by_manager/route.js
+++ src/app/api/weekly_stats_by_manager/route.js
@@ -130,8 +130,8 @@ export async function POST(req) {
       result.push({
         manager_id: managerId,
         team_name: managerData?.team_name || `Manager #${managerId}`,
-        batters: { ...batterSum, AVG, OPS },
-        pitchers: { ...pitcherSum, ERA, WHIP, IP: `${Math.floor(IP)}.${pitcherSum.OUT % 3}` }
+        batters: { ...batterSum, AVG, OPS, fantasyPoints: {} },     // 加上空殼
+        pitchers: { ...pitcherSum, ERA, WHIP, IP: `${Math.floor(IP)}.${pitcherSum.OUT % 3}`, fantasyPoints: {} }
       })
     }
 

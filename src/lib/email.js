e5ddde3de29a5c3709946f1cc48d84e9714b3a1c
Laiyuhung <laiyuhung921118@gmail.com>
2025-04-29 15:36:22 +0800

    更新0429

--- src/lib/email.js
+++ src/lib/email.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 export async function sendTradeNotificationEmail(to, subject, html) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Fantasy Baseball <your@email.com>', // 這個 from 要跟 Resend 驗證的 domain 有關
+      from: 'Fantasy Baseball <onboarding@resend.dev>', // 這個 from 要跟 Resend 驗證的 domain 有關
       to,
       subject,
       html,

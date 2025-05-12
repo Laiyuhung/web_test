04a595ab6a2b2f2155ee07faaa0f7813a33576b5
Laiyuhung <laiyuhung921118@gmail.com>
2025-05-12 09:34:47 +0800

    0512

--- src/app/api/email/send/route.js
+++ src/app/api/email/send/route.js
@@ -2,10 +2,6 @@
 import { sendTradeNotificationEmail } from '@/lib/email'
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' })
-  }
-
   const { to, subject, html } = req.body
 
   try {

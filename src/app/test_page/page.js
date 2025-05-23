f710b6ebd284c9b31e0df4baa929e6e2796f501c
Laiyuhung <laiyuhung921118@gmail.com>
2025-05-23 14:59:38 +0800

    0523

--- src/app/test_page/page.js
+++ src/app/test_page/page.js
@@ -17,11 +17,9 @@ export default function Home() {
 
         <span className="text-3xl font-semibold w-12 text-center">{count}</span>
 
-        <button
-          onClick={() => setCount(count + 1)}
-          className="px-4 py-2 rounded-full shadow-md bg-white hover:bg-gray-100 border border-gray-300 text-xl font-bold"
-        >
-          +
+        <button className="flex items-center space-x-2 px-4 py-2 rounded-lg bg-[#5D4037] hover:bg-[#4E342E] shadow">
+          <Cog6ToothIcon className="w-6 h-6 text-white" />
+          <span className="text-white font-medium">+</span>
         </button>
       </div>
 
